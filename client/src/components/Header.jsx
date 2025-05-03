15ccda845a0227a6ab56ad6c6eb03c35a532d674
Aditya Gupta <87805459+impulseadi@users.noreply.github.com>
2025-05-03 11:47:26 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
     <motion.div className='flex flex-col justify-center items-center text-center my-20' initial={{opacity:0.2, y:100}} transition={{duration :1}} whileInView={{opacity:1, y:0}} viewport={{once:true}}>
       <motion.div className='text-stone-500 inline-flex text-center gap-2 bg-white px-2 py-1 rounded-full border border-neutral-500'  initial={{opacity:0.2, y:-20}} transition={{delay:0.2,duration :0.8}} animate={{opacity:1, y:0}}>
         <p>Best text to image generator</p>
-        <img src={assets.star_icon} alt="" />
+        <img loading="lazy" src={assets.star_icon} alt="" />
       </motion.div>
       <motion.h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center' initial={{ opacity:0 }}
         animate={{opacity:1}}
@@ -38,13 +38,13 @@ const Header = () => {
         initial={{ opacity:0 }}
         animate={{ opacity:1 }}
         transition={{default: {duration:0.5}, opacity: { delay:0.8, duration:1 } }}>Generate Images 
-        <img className='h-6' src={assets.star_group} alt="" /></motion.button>
+        <img loading="lazy" className='h-6' src={assets.star_group} alt="" /></motion.button>
    
     <motion.div initial={{ opacity:0 }}
         animate={{opacity:1}}
         transition={{delay:1, duration:1}} className='flex flex-wrap justify-center mt-16 gap-3'>
         {Array(6).fill('').map((item, index)=>(
-            <img className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10' 
+            <img loading="lazy" className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10' 
             src={index % 2===0 ? assets.sample_img_1 : assets.sample_img_2} alt="" key={index} width={70}/>
         ))}
     </motion.div>
