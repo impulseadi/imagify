83e2df2d85fcd0be1ce562ebe40d58a207cc370a
Aditya Gupta <87805459+impulseadi@users.noreply.github.com>
2025-05-03 11:52:48 +0530

    Update Testimonials.jsx

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets, testimonialsData } from "../assets/assets"
 import { motion } from 'framer-motion'
-const Testimonials = () => {
+const Testimonials = React.memo(() => {
   return (
     <motion.div  initial={{ opacity: 0.2, y: 100 }}
     transition={{ duration: 1}}
@@ -24,7 +24,7 @@ const Testimonials = () => {
                 cursor-pointer hover:scale-[1.02] transition-all">
                     <div className="flex flex-col items-center">
                         <img src={testimonial.image} alt="" 
-                        className="rounded-full w-14" />
+                        className="rounded-full w-14" loading="lazy"  />
                         <h2 className="text-xl font-semibold mt-3">{testimonial.name}</h2>
                         <p className="text-gray-500 mb-4">{testimonial.role}</p>
                         <div className="flex mb-4">
