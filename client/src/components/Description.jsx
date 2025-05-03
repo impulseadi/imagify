b16c1d828956856d32c9c56bfecfdfd43a89484e
Aditya Gupta <87805459+impulseadi@users.noreply.github.com>
2025-05-03 11:48:04 +0530

    Update Description.jsx

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -15,7 +15,7 @@ const Description = () => {
 
       <div className="flex flex-col gap-5 md:gap-14 
         md:flex-row items-center">
-        <img  className="w-80 xl:w-96 rounded-lg" src={assets.sample_img_1} alt="" />
+        <img loading="lazy" className="w-80 xl:w-96 rounded-lg" src={assets.sample_img_1} alt="" />
         <div>
             <h2 className="text-3xl font-medium max-w-lg mb-4">Introducing the AI-Powered Text to Image Generator</h2>
             <p className="text-gray-600 mb-4">
